cdd35f3431b709ec82d22377366bf2747ef08404
imdevponce <imdevponce@gmail.com>
2024-03-01 21:31:23 -0600

    added home route

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+"use client"
+import { NavContainer } from "./header.styles"
+export default function Nav() {
+    return (
+        <NavContainer>Nav</NavContainer>
+    );
+  }
+  
---
+++ src/components/header.styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components"
+
+export const NavContainer = styled.header `
+    height: 80px;
+    borderBottom: 1px solid #d9d9d9;
+`
