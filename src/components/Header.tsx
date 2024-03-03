246e12c7a7db08ca10c7d17bc9f65db935d165cf
imdevponce <imdevponce@gmail.com>
2024-03-02 19:58:33 -0600

    added eslint and nav bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,25 @@
-"use client"
-import { NavContainer } from "./header.styles"
-export default function Nav() {
-    return (
-        <NavContainer>Nav</NavContainer>
-    );
-  }
-  
+'use client'
+import { NavContainer, NavLink, LeftSectionInNav } from './header.styles'
+import Image from 'next/image'
+
+export default function Nav (): JSX.Element {
+  return (
+    <NavContainer>
+      <LeftSectionInNav>
+        <NavLink href="/">
+          <Image src="/logo.png" alt="Celebrast logo" width={100} height={100}/>
+        </NavLink>
+        <nav>
+          <NavLink href="/mi-celebracion">Mi celebración</NavLink>
+          <NavLink href="/proveedores">Proveedores</NavLink>
+          <NavLink href="/ser-proveedor">¿Cómo ser proveedor?</NavLink>
+          <NavLink href="/solicitudes">Solicitudes recientes</NavLink>
+          <NavLink href="/login">¿Cómo ser proveedor?</NavLink>
+        </nav>
+      </LeftSectionInNav>
+      <NavLink href="/login">
+        Login
+      </NavLink>
+    </NavContainer>
+  )
+}
