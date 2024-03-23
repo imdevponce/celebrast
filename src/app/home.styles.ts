2d6f336e82619086f483ffde4902f3cb4f93d484
imdevponce <imdevponce@gmail.com>
2024-03-23 17:55:41 -0600

    responsive design added home page

--- src/app/home.styles.ts
+++ src/app/home.styles.ts
@@ -8,25 +8,65 @@ export const MainHomeContainer = styled.div`
      width: 100vw;
      height: 350px;
      border-bottom: 0.5px solid #d9d9d9;
+     @media (max-width: 1335px) {
+        height: 400px;
+    }
+    @media (max-width: 768px) {
+        height: 600px;
+    }
 `
 export const SearchContainer = styled.div`
     padding: 20px 65px;
     position: absolute;
     top: 25%;
+    display: flex;
+    flex-direction: column;
     h1 {
         font-size: 24px;
+        font-weight: 600;
+        width: 500px;
+        @media (max-width: 768px) {
+            font-size: 18px;
+            width: 350px;
+            margin: 0 auto;
+
+        }
+        @media (max-width: 425px) {
+                width: 250px;
+        }
     }
-    width: 500px;
-    @media screen and (max-width: 1024px) {
-        width: 100%;
-        top: 40%;
+    @media (max-width: 1335px) {
+        top: 70%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
     }
+    form {
+        @media (max-width: 768px) {
+            flex-direction: column;
+        }
+        input, button {
+            @media (max-width: 768px) {
+                width: 400px;
+                margin: 5px auto;
+            }
+            @media (max-width: 425px) {
+                width: 300px;
+            }
+            @media (max-width: 320px) {
+                width: 250px;
+            }
+        }
+    }
+    
+  
 `
 export const SearchFormContainer = styled.form`
     display: flex;
     margin-top: 20px;
 `
 export const AboutCelebrastContainer = styled.div`
+    padding: 0px 0px 30px 0px;
 `
 export const AboutCelebrastTitle = styled.h2`
     background-color: #FFFFFF;
@@ -34,5 +74,46 @@ export const AboutCelebrastTitle = styled.h2`
     padding-bottom: 30px;
     width: 100vw;
     padding: 30px 65px 30px 65px;
+    font-weight: 500;
+    strong {
+        color: #57C2E2
+    }
+`
 
+export const ClientSectionContainer = styled.section`
+    padding: 0 65px 0 65px;
+    text-align: center;
+    h2 {
+        font-weight: 500;
+        strong {
+            color: #57C2E2
+        }
+    }
+`
+
+export const CelebrastDescription = styled.p`
+        width: 70%;
+        margin: 0 auto;
+        margin-top: 20px;
+        min-width: 200px;
+`
+export const ClientSteps = styled.div`
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+`
+export const StepContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 10px;
+    width: 250px;
+    height: 250px;
+    h3 {
+        margin-top: 10px;
+    }
+    b {
+        margin-bottom: 10px;
+    }
 `
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,31 @@
 'use client'
-import { HomeContainer, SearchContainer, MainHomeContainer, SearchFormContainer, AboutCelebrastContainer, AboutCelebrastTitle } from './home.styles'
+import { HomeContainer, SearchContainer, MainHomeContainer, SearchFormContainer, AboutCelebrastContainer, AboutCelebrastTitle, ClientSectionContainer, CelebrastDescription, ClientSteps, StepContainer } from './home.styles'
 import Image from 'next/image'
 import Input from 'app/components/Input'
 import Button from 'app/components/Button'
 import styles from './home.module.css'
+import useViewportWidth from 'app/app/hooks/useViewportWidth'
+
 export default function Home () {
+  const width = useViewportWidth()
+  console.log('width', width)
   return (
     <HomeContainer>
       <MainHomeContainer >
-        <Image
-          src='/table-setting-desktop.png'
-          alt="Table setting"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="center"/>
+        {width <= 1335
+          ? <Image
+            src='/table-setting-mobile.png'
+            alt="Mesa"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"/>
+          : <Image
+            src='/table-setting-desktop.png'
+            alt="Mesa"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"/>}
+
         <SearchContainer>
           <h1>¡Bienvenido! Encuentra todos los proveedores para tu celebración especial en un solo lugar.</h1>
           <SearchFormContainer>
@@ -25,16 +37,16 @@ export default function Home () {
       </MainHomeContainer>
       <AboutCelebrastContainer>
         <AboutCelebrastTitle>
-          Puedes organizar de todo con Celebrast!
+          Puedes organizar de todo con <strong>CELEBRAST</strong>!
         </AboutCelebrastTitle>
-        <div className={styles.container}>
+        <section className={styles.container}>
           <div className={styles.collage_wrap}>
             <div className={styles.collage_item}>
               <div className={styles.collage_item_inner}>
                 <Image src='/baby-shower.png'
                   layout="fill"
                   objectFit="cover"
-                  objectPosition="center" alt='g'/>
+                  objectPosition="center" alt='Baby shower'/>
               </div>
             </div>
             <div className={styles.collage_item}>
@@ -42,7 +54,7 @@ export default function Home () {
                 <Image src='/birthday.png'
                   layout="fill"
                   objectFit="cover"
-                  objectPosition="center" alt='g'/>
+                  objectPosition="center" alt='Cumpleaños'/>
               </div>
             </div>
             <div className={styles.collage_item}>
@@ -50,7 +62,7 @@ export default function Home () {
                 <Image src='/table-setting-2.png'
                   layout="fill"
                   objectFit="cover"
-                  objectPosition="center" alt='g'/>
+                  objectPosition="center" alt='Mesa'/>
               </div>
             </div>
             <div className={styles.collage_item}>
@@ -58,7 +70,7 @@ export default function Home () {
                 <Image src='/wedding.png'
                   layout="fill"
                   objectFit="cover"
-                  objectPosition="center" alt='g'/>
+                  objectPosition="center" alt='Boda'/>
               </div>
             </div>
             <div className={styles.collage_item}>
@@ -66,11 +78,60 @@ export default function Home () {
                 <Image src='/xv.png'
                   layout="fill"
                   objectFit="cover"
-                  objectPosition="center" alt='g'/>
+                  objectPosition="center" alt='Quince años'/>
               </div>
             </div>
           </div>
-        </div>
+        </section>
+        <ClientSectionContainer>
+          <h2>¿Cómo funciona <strong>CELEBRAST</strong>?</h2>
+          <CelebrastDescription>
+              ¡Celebra con facilidad en <strong>CELEBRAST</strong>! Tu plataforma digital para la planificación de eventos. Encuentra proveedores de fotografía, salones de eventos, buffets y más, ¡todo en un solo lugar! Haz realidad tus sueños de celebración con nosotros.
+          </CelebrastDescription>
+          <ClientSteps>
+            <StepContainer>
+              <b>1</b>
+              <Image
+                src='/search.png'
+                alt="Busqueda"
+                width={150}
+                height={150}
+              />
+              <h3>Encuentra tu proveedor</h3>
+            </StepContainer>
+            <StepContainer>
+              <b>2</b>
+              <Image
+                src='/message.png'
+                alt="Mensaje"
+                width={150}
+                height={150}
+              />
+              <h3>Contáctalo</h3>
+
+            </StepContainer>
+            <StepContainer>
+              <b>3</b>
+              <Image
+                src='/details.png'
+                alt="Detalles"
+                width={150}
+                height={150}
+              />
+              <h3>Acuerda los detalles</h3>
+            </StepContainer>
+            <StepContainer>
+              <b>4</b>
+              <Image
+                src='/deal.png'
+                alt="Acuerdo"
+                width={150}
+                height={150}
+              />
+              <h3>Realiza tu contratación</h3>
+            </StepContainer>
+          </ClientSteps>
+        </ClientSectionContainer>
       </AboutCelebrastContainer>
     </HomeContainer>
   )
