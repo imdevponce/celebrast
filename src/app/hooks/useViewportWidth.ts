0a71ba3ec495cba16f44f03093b4b8196a7ec79a
imdevponce <imdevponce@gmail.com>
2024-03-23 18:19:37 -0600

    use client hook

--- src/app/hooks/useViewportWidth.ts
+++ src/app/hooks/useViewportWidth.ts
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react'
 
 export default function useViewportWidth () {
-  const [width, setWidth] = useState(window.innerWidth)
+  const [width, setWidth] = useState(1)
 
   useEffect(() => {
     const handleResize = () => {
